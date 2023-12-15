aa5482d62aab13b2e3fd3d884bc778fcccb08710
Federico Slongo <federicoslongo@gmail.com>
2023-12-15 20:06:57 +0100

    Fullscreen button update and separeted files

---
+++ javascript.js
@@ -0,0 +1,34 @@
+/* Get the documentElement (<html>) to display the page in fullscreen */
+var elem = document.documentElement;
+var clicked = false;
+
+function changeState(){
+    if(clicked)
+        closeFullscreen();
+    else
+        openFullscreen();
+
+    clicked = !clicked;
+}
+
+/* View in fullscreen */
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE11 */
+    elem.msRequestFullscreen();
+  }
+}
+
+/* Close fullscreen */
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) { /* Safari */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) { /* IE11 */
+    document.msExitFullscreen();
+  }
+}
