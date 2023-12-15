e9ca395cde5577e688f226451410b823ead9048d
Federico Slongo <federicoslongo@gmail.com>
2023-12-15 20:36:55 +0100

    Time

--- javascript.js
+++ javascript.js
@@ -1,3 +1,22 @@
+var dateVar;
+
+// Calculate milliseconds in a year
+const minute = 1000 * 60;
+const hour = minute * 60;
+const day = hour * 24;
+const year = day * 365;
+
+function updateTime() {
+    setTimeout(function() {
+        dateVar = new Date();
+        var time = dateVar.getTime();
+        document.getElementById('date').innerText = (dateVar.getDay()) + " - " + (dateVar.getMonth() + 1) + " - " + (dateVar.getFullYear() + 1);
+        document.getElementById('fineDate').innerText = dateVar.getHours() + " - " + dateVar.getMinutes() + " - " + (dateVar.getSeconds() < 10 ? "0" +  dateVar.getSeconds(): dateVar.getSeconds());
+        updateTime();
+    }, 100);
+}
+
+
 /* Get the documentElement (<html>) to display the page in fullscreen */
 var elem = document.documentElement;
 var clicked = false;
@@ -20,6 +39,10 @@ function openFullscreen() {
   } else if (elem.msRequestFullscreen) { /* IE11 */
     elem.msRequestFullscreen();
   }
+
+  document.getElementById("fullscreenButton").style.display = "none";
+  document.getElementById("fullscreenButton").style.height = "1px";
+  document.getElementById("fullscreenButton").style.width = "1px";
 }
 
 /* Close fullscreen */
