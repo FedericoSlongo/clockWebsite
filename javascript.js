c3d2f1c4cf8e25fc8a939000cc6eaf379bd4d8dc
Federico Slongo <federicoslongo@gmail.com>
2023-12-17 20:20:24 +0100

    Fixed formating is some stuff :)

--- javascript.js
+++ javascript.js
@@ -3,27 +3,32 @@ var dateVar;
 
 /* Function that updates time */
 function updateTime() {
-    /* Waits for a second before updating the time */
-    setTimeout(function() {
-        /* Gets the current date from the system */
-        dateVar = new Date();
-        /* dd - mm - yyyy */
-        document.getElementById('date').innerText = (dateVar.getDate() < 10 ? "0" +  dateVar.getDate() : dateVar.getDate()) + " - " + ((dateVar.getMonth() + 1) < 10 ? "0" +  (dateVar.getMonth() + 1) : (dateVar.getMonth() + 1) + " - " + (dateVar.getFullYear() + 1));
-        /* h - m - s */
-        document.getElementById('fineDate').innerText = (dateVar.getHours() < 10 ? "0" +  dateVar.getHours() : dateVar.getHours()) + " - " + (dateVar.getMinutes() < 10 ? "0" +  dateVar.getMinutes() : dateVar.getMinutes()) + " - " + (dateVar.getSeconds() < 10 ? "0" +  dateVar.getSeconds() : dateVar.getSeconds());
-        updateTime();
-    }, 100);
+  /* Waits for a second before updating the time */
+  setTimeout(function() {
+    /* Gets the current date from the system */
+    dateVar = new Date();
+    /* dd - mm - yyyy */
+    document.getElementById('date').innerText = (dateVar.getDate() < 10 ? "0" +  dateVar.getDate() : dateVar.getDate()) + " - " + ((dateVar.getMonth() + 1) < 10 ? "0" +  (dateVar.getMonth() + 1) : (dateVar.getMonth() + 1) + " - " + (dateVar.getFullYear() + 1));
+    /* h - m - s */
+    document.getElementById('fineDate').innerText = (dateVar.getHours() < 10 ? "0" +  dateVar.getHours() : dateVar.getHours()) + " - " + (dateVar.getMinutes() < 10 ? "0" +  dateVar.getMinutes() : dateVar.getMinutes()) + " - " + (dateVar.getSeconds() < 10 ? "0" +  dateVar.getSeconds() : dateVar.getSeconds());
+    updateTime();
+  }, 100);
 }
 
 
 /* Function to Hide and UnHide the fullscreen button */
 function unHideButton(){
-    document.getElementById("fullscreenButton").style.background = "rgba(0, 0, 0, 0.3)";
-    document.getElementById("fullscreenButton").style.color = "#f1f1f1";
+  document.getElementById("fullscreenButton").style.background = "rgba(0, 0, 0, 0.3)";
+  document.getElementById("fullscreenButton").style.color = "#f1f1f1";
 }
 function hideButton(){
-    document.getElementById("fullscreenButton").style.background = "rgba(0, 0, 0, 0)";
-    document.getElementById("fullscreenButton").style.color = "rgba(0, 0, 0, 0)";
+  document.getElementById("fullscreenButton").style.background = "rgba(0, 0, 0, 0)";
+  document.getElementById("fullscreenButton").style.color = "rgba(0, 0, 0, 0)";
+}
+
+/* Function to change the background (Not Working) */
+function changeVideoBackground(){
+  document.getElementById("videoBackground").src = "./background/video/[REDACTED].mp4";
 }
 
 
@@ -32,12 +37,12 @@ var elem = document.documentElement;
 var clicked = false;
 
 function changeState(){
-    if(clicked)
-        closeFullscreen();
-    else
-        openFullscreen();
+  if(clicked)
+      closeFullscreen();
+  else
+      openFullscreen();
 
-    clicked = !clicked;
+  clicked = !clicked;
 }
 
 /* View in fullscreen */
